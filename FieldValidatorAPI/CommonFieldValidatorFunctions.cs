46f9ba4cbec1d253f3b9a1190fc2a8fb9a945cdb
Daffa Azka <destrcode@gmail.com>
2023-12-05 19:42:09 +0700

    v0.2
    Adding Validator API

---
+++ FieldValidatorAPI/CommonFieldValidatorFunctions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FieldValidatorAPI
+{
+    public delegate bool RequiredValidGate(string fieldVal);
+    public delegate bool PatternMatchValidGate(string fieldVal, string pattern);
+    public delegate bool CompareFieldsValidGate(string fieldVal, string fieldCompare);
+    public class CommonFieldValidatorFunctions
+    {
+        private static RequiredValidGate _requiredValidGate = null;
+        private static PatternMatchValidGate _patternMatchValidGate = null;
+        private static CompareFieldsValidGate _compareFieldsValidGate = null;
+
+        public static RequiredValidGate RequiredValidGate
+        {
+            get
+            {
+                if (_requiredValidGate == null)
+                {
+                    _requiredValidGate = new RequiredValidGate(RequiredValid);
+                }
+                return _requiredValidGate;
+            }
+        }
+
+        public static PatternMatchValidGate PatternMatchValidGate
+        {
+            get
+            {
+                if (_patternMatchValidGate == null)
+                {
+                    _patternMatchValidGate = new PatternMatchValidGate(PatternMatchValid);
+                }
+                return _patternMatchValidGate;
+            }
+        }
+
+        public static CompareFieldsValidGate CompareFieldsValidGate
+        {
+            get
+            {
+                if (_compareFieldsValidGate == null)
+                {
+                    _compareFieldsValidGate = new CompareFieldsValidGate(CompareFieldsValid);
+                }
+                return CompareFieldsValidGate;
+            }
+        }
+
+
+
+
+        private static bool RequiredValid(string fieldVal)
+        {
+            return !string.IsNullOrEmpty(fieldVal);
+        }
+
+        private static bool PatternMatchValid(string fieldVal, string regexVal)
+        {
+            Regex regex = new Regex(regexVal);
+            return regex.IsMatch(fieldVal);
+        }
+
+        private static bool CompareFieldsValid(string fieldVal, string fieldCompare)
+        {
+            return fieldVal.Equals(fieldCompare);
+        }
+    }
+}
---
+++ FieldValidatorAPI/CommonRegularExpressionValidationPatterns.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldValidatorAPI
+{
+    public static class CommonRegularExpressionValidationPatterns
+    {
+        public const string Email_RegEx_Pattern = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+        public const string Password_RegEx_Pattern = @"(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$";
+        public const string Username_RegEx_Pattern = @"/^\S*$/;";
+    }
+}
